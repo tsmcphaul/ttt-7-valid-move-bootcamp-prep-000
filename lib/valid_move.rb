7f8a8f9480aaba26b31d1cdd8c7776a4d43da52d
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 22:57:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
 
 def position_taken?(board, index)
-  if board[index]=="" || board[index]==" " || board[index]== nil
+  if board[index] =="" || board[index] ==" " || board[index] == nil
     return false
   else 
     return true
