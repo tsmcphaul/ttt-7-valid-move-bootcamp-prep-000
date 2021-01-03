2f158c1ecfcde3cb7ef590feeea3792fabd0b004
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 22:04:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,6 @@
 # code your #valid_move? method here
 def valid_move?
-  if 
-end  
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index]=="" || board[index]==" " || board[index]== nil
     return false
@@ -11,3 +8,12 @@ def position_taken?(board, index)
     return true
 end
 end
+
+
+
+
+
+
+
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
