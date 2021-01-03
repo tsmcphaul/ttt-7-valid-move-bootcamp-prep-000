076c7d57ebc62276f3a09c72ad55cd667fc61edf
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 22:04:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-def valid_move?
+def valid_move?(board, index)
 
 def position_taken?(board, index)
   if board[index]=="" || board[index]==" " || board[index]== nil
